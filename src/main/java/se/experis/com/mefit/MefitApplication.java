0e0d168300c61277db06af5446d9c5759474f1a2
GustafHasselgren <gustaf.ericson2@gmail.com>
2023-08-28 16:05:16 +0200

    first commit

---
+++ src/main/java/se/experis/com/mefit/MefitApplication.java
@@ -0,0 +1,13 @@
+package se.experis.com.mefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MefitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MefitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/experis/com/mefit/MefitApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis.com.mefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MefitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
