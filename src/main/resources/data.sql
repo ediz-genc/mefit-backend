230bf95408404adb18bcd6f04b4f197ea709e710
GustafHasselgren <gustaf.ericson2@gmail.com>
2023-08-30 11:33:22 +0200

    dev: relations complete

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -43,3 +43,15 @@ VALUES
 INSERT INTO tb_goal_user (goal_goal_id, user_user_id)
 VALUES
 (1,1), (2,9), (3,7), (1,8), (2,5);
+
+INSERT INTO tb_program_goals (goals_goal_id, programs_program_id)
+VALUES 
+(1,1), (1,2), (2,1), (3,2);
+
+INSERT INTO tb_workout_goals (goals_goal_id, workouts_workout_id)
+VALUES 
+(1,3), (2,1), (3,1), (1,2), (2,3), (3,2);
+
+INSERT INTO tb_workout_programs (workouts_workout_id, programs_program_id)
+VALUES
+(1,1), (2,1), (3,1), (1,2), (2,2), (3,2);
