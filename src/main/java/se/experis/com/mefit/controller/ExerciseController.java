f88a705e065fc7e7aafb8ee0c12b7540de180597
“linusJtareby” <linus.tareby@se.experis.com>
2023-08-30 15:52:33 +0200

    basic crudcontrollers: Workout, program, exercise

--- src/main/java/se/experis/com/mefit/controller/ExerciseController.java
+++ src/main/java/se/experis/com/mefit/controller/ExerciseController.java
@@ -1,13 +1,19 @@
 package se.experis.com.mefit.controller;
 
+import java.net.URI;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import se.experis.com.mefit.model.Exercise;
 import se.experis.com.mefit.service.ExerciseService;
@@ -23,9 +29,32 @@ public class ExerciseController {
         this.exerciseService = exerciseService;
     }
 
+    @Operation(description = "Get all exercises")
     @GetMapping
     public ResponseEntity<Set<Exercise>> getAll() {
         Set<Exercise> exercises = exerciseService.findAll();
         return ResponseEntity.ok(exercises);
+    }
+
+    @Operation(description = "Get exercise with given id")
+    @GetMapping("{id}")
+    public ResponseEntity<Exercise> getById(@PathVariable int id) {
+        Exercise exercise = exerciseService.findById(id);
+        return ResponseEntity.ok(exercise);
+    }
+
+    @Operation(description = "Add a new exercise")
+    @PostMapping
+    public ResponseEntity<Exercise> addExercise(@RequestBody Exercise exercise) {
+        Exercise newExercise = exerciseService.add(exercise);
+        URI location = URI.create("/exercise" + newExercise.getId());
+        return ResponseEntity.created(location).build();
+    }
+
+    @Operation(description = "Delete an exercise with a given id")
+    @DeleteMapping("{id}")
+    public ResponseEntity<Exercise> deleteExercise(@PathVariable int id) {
+        exerciseService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/se/experis/com/mefit/controller/ProgramController.java
+++ src/main/java/se/experis/com/mefit/controller/ProgramController.java
@@ -1,12 +1,20 @@
 package se.experis.com.mefit.controller;
 
+import java.net.URI;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import se.experis.com.mefit.model.Program;
 import se.experis.com.mefit.service.ProgramService;
@@ -22,9 +30,40 @@ public class ProgramController {
         this.programService = programService;
     }
 
+    @Operation(description = "Get all programs")
     public ResponseEntity<Set<Program>> getAll() {
         Set<Program> programs = programService.findAll();
         return ResponseEntity.ok(programs);
+    }
+
+    @Operation(description = "Get a program by given id")
+    @GetMapping("{id}")
+    public ResponseEntity<Program> getById(@PathVariable int id) {
+        Program program = programService.findById(id);
+        return ResponseEntity.ok(program);
+    }
+
+    @Operation(description = "Add new program")
+    @PostMapping
+    public ResponseEntity<Program> addWorkout(@RequestBody Program program) {
+        Program newProgram = programService.add(program);
+        URI location = URI.create("/program" + newProgram.getId());
+        return ResponseEntity.created(location).build();
+    }
+
+    @Operation(description = "Update existing program with given id")
+    @PutMapping("{id}")
+    public ResponseEntity<Program> updateProgram(@PathVariable int id, @RequestBody Program program) {
+        Program updatedProgram = programService.update(id, program);
+        programService.add(updatedProgram);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(description = "Delete program with given id")
+    @DeleteMapping("{id}")
+    public ResponseEntity<Program> deleteProgram(@PathVariable int id) {
+        programService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
