4e5b509e9b88e535ee04fadd0ed0a1efe68716e5
ediz.genc <ediz.genc@yahoo.com>
2023-08-31 03:25:37 +0200

    dev: keycloak backend endpoints (public, restricted, admin)

---
+++ src/main/java/se/experis/com/mefit/config/JwtAuthConverter.java
@@ -0,0 +1,71 @@
+package se.experis.com.mefit.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/*
+ * Convert a JSON Web Token (JWT) into an authentication token that can be used for
+ * authentication and authorization within a Spring-based application.
+ *
+ * A bridge between the JWT-based authentication system and the Spring Security framework.
+ * Extracts relevant user roles and resource-specific roles from the JWT and constructs an
+ * authentication token that can be utilized by Spring Security to make informed decisions
+ * about authentication and authorization for the user.
+ */
+
+@Component
+public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    @Value("${keycloak.client-id}")
+    private String clientId;
+
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+    @Override
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                extractResourceRoles(jwt).stream()).collect(Collectors.toSet());
+
+        return new JwtAuthenticationToken(
+                jwt,
+                authorities,
+                jwt.getClaim(JwtClaimNames.SUB));
+    }
+
+    @SuppressWarnings("unchecked")
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess;
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+        if (jwt.getClaim("resource_access") == null) {
+            return Set.of();
+        }
+        resourceAccess = jwt.getClaim("resource_access");
+
+        if (resourceAccess.get(clientId) == null) {
+            return Set.of();
+        }
+        resource = (Map<String, Object>) resourceAccess.get(clientId);
+
+        resourceRoles = (Collection<String>) resource.get("roles");
+        return resourceRoles
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
+    }
+}
---
+++ src/main/java/se/experis/com/mefit/controller/EndpointController.java
@@ -0,0 +1,33 @@
+package se.experis.com.mefit.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/v1/resources")
+public class EndpointController {
+
+    @GetMapping("public")
+    public ResponseEntity<String> publicEndpoint() {
+        return new ResponseEntity<>(
+                "A public endpoint, accessible anonymously.",
+                HttpStatus.OK);
+    }
+
+    @GetMapping("restricted")
+    public ResponseEntity<String> restrictedEndpoint() {
+        return new ResponseEntity<>(
+                "A restricted endpoint, accessible only when logged in.",
+                HttpStatus.OK);
+    }
+
+    @GetMapping("admin")
+    public ResponseEntity<String> adminEndpoint() {
+        return new ResponseEntity<>(
+                "Admin endpoint, accessible only for admins.",
+                HttpStatus.OK);
+    }
+}
