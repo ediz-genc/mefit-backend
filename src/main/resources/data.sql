d0b8d323dd59931d38b199d525f6edd8ad02122b
“linusJtareby” <linus.tareby@se.experis.com>
2023-09-19 10:11:09 +0200

    data.sql: added img and exercises to all workouts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,75 +1,108 @@
-INSERT INTO tb_user (user_id, user_length, user_weight, user_bio, user_pic_url, user_username)
-VALUES
-    ('58efed46-d459-4a14-b6bb-dec9b4240f94', 170, 65, 'Fitness enthusiast and nature lover.', 'https://example.com/user1.jpg', 'fitNatureLover'),
-    ('3542EF3', 160, 55, 'Foodie and traveler.', 'https://example.com/user2.jpg', 'wanderlustFoodie'),
-    ('23#45gs', 175, 70, 'Tech geek and aspiring astronaut.', 'https://example.com/user3.jpg', 'techSpaceFan'),
-    ('adf#%%r', 180, 80, 'Bookworm and coffee addict.', 'https://example.com/user4.jpg', 'bookishBrew'),
-    ('geg#%&d', 165, 50, 'Artist and dreamer.', 'https://example.com/user5.jpg', 'creativeDream'),
-    ('#ea#345', 172, 68, 'Sports lover and adventurer.', 'https://example.com/user6.jpg', 'activeExplorer'),
-    ('dfa245#', 155, 48, 'Musician and night owl.', 'https://example.com/user7.jpg', 'melodicNight'),
-    ('345<adf', 185, 75, 'Animal lover and environmentalist.', 'https://example.com/user8.jpg', 'ecoAnimalFriend'),
-    ('aaf#¤ae', 168, 58, 'Fashionista and travel enthusiast.', 'https://example.com/user9.jpg', 'stylishJourney'),
-    ('dsus#%4', 178, 72, 'Cooking enthusiast and aspiring chef.', 'https://example.com/user10.jpg', 'culinaryArtist');
-
 INSERT INTO tb_exercise (exercise_name, muscle_group, exercise_desc, exercise_img_url, exercise_vid_url, fitness_lvl)
 VALUES
-    ('Push-up', 'Chest', 'A classic bodyweight exercise targeting the chest and triceps.', 'https://example.com/exercise1.jpg', 'https://example.com/exercise1.mp4', 'Beginner'),
-    ('Squat', 'Legs', 'A fundamental lower body exercise to strengthen the quadriceps and glutes.', 'https://example.com/exercise2.jpg', 'https://example.com/exercise2.mp4', 'Intermediate'),
-    ('Pull-up', 'Back', 'An upper body exercise that targets the back and biceps.', 'https://example.com/exercise3.jpg', 'https://example.com/exercise3.mp4', 'Advanced'),
-    ('Plank', 'Core', 'An isometric core exercise that also engages the shoulders and chest.', 'https://example.com/exercise4.jpg', 'https://example.com/exercise4.mp4', 'Beginner'),
-    ('Deadlift', 'Back', 'A compound exercise targeting the back, hamstrings, and core.', 'https://example.com/exercise5.jpg', 'https://example.com/exercise5.mp4', 'Intermediate'),
-    ('Lunges', 'Legs', 'A unilateral leg exercise that helps improve lower body strength and balance.', 'https://example.com/exercise6.jpg', 'https://example.com/exercise6.mp4', 'Intermediate'),
-    ('Bicep Curl', 'Arms', 'An isolation exercise to target the biceps.', 'https://example.com/exercise7.jpg', 'https://example.com/exercise7.mp4', 'Beginner'),
+    ('Push-up', 'Chest', 'A classic bodyweight exercise targeting the chest and triceps.', 'https://www.fitnesseducation.edu.au/wp-content/uploads/2020/10/Pushups.jpg', 'https://example.com/exercise1.mp4', 'Beginner'),
+    ('Squat', 'Legs', 'A fundamental lower body exercise to strengthen the quadriceps and glutes.', 'https://hips.hearstapps.com/hmg-prod/images/squat-jump-squat-178-1653334247.jpg?crop=0.859xw:0.646xh;0.0481xw,0.342xh&resize=1200:*', 'https://example.com/exercise2.mp4', 'Intermediate'),
+    ('Pull-up', 'Back', 'An upper body exercise that targets the back and biceps.', 'https://m2.apuls.dk/media/wysiwyg/blog/illustration/u05-bottomhalfwaytop-ism-mh310118-1558552383.jpg', 'https://example.com/exercise3.mp4', 'Advanced'),
+    ('Plank', 'Core', 'An isometric core exercise that also engages the shoulders and chest.', 'https://hips.hearstapps.com/hmg-prod/images/hdm119918mh15842-1545237096.png?crop=0.668xw:1.00xh;0.117xw,0&resize=1200:*', 'https://example.com/exercise4.mp4', 'Beginner'),
+    ('Deadlift', 'Back', 'A compound exercise targeting the back, hamstrings, and core.', 'https://hips.hearstapps.com/hmg-prod/images/trap-bar-deadlift-1618411658.jpg', 'https://example.com/exercise5.mp4', 'Intermediate'),
+    ('Lunges', 'Legs', 'A unilateral leg exercise that helps improve lower body strength and balance.', 'https://i0.wp.com/post.healthline.com/wp-content/uploads/2019/09/woman-doing-frontal-lunges-indoors-in-a-flat-1296x728-header-1296x728.jpg?w=1155&h=1528', 'https://example.com/exercise6.mp4', 'Intermediate'),
+    ('Bicep Curl', 'Arms', 'An isolation exercise to target the biceps.', 'https://experiencelife.lifetime.life/wp-content/uploads/2021/08/f2-biceps-curl.jpg', 'https://example.com/exercise7.mp4', 'Beginner'),
     ('Plank Twist', 'Core', 'A variation of the plank exercise that engages the obliques.', 'https://example.com/exercise8.jpg', 'https://example.com/exercise8.mp4', 'Intermediate'),
-    ('Dips', 'Arms', 'An upper body exercise targeting the triceps and shoulders.', 'https://example.com/exercise9.jpg', 'https://example.com/exercise9.mp4', 'Advanced'),
-    ('Russian Twist', 'Core', 'An abdominal exercise that involves twisting motions.', 'https://example.com/exercise10.jpg', 'https://example.com/exercise10.mp4', 'Intermediate');
+    ('Dips', 'Arms', 'An upper body exercise targeting the triceps and shoulders.', 'https://femina.wwmindia.com/thumb/photogallery/2016/Sep/thumbnail_1474020779.jpg?width=1200&height=900', 'https://example.com/exercise9.mp4', 'Advanced'),
+    ('Russian Twist', 'Core', 'An abdominal exercise that involves twisting motions.', 'https://cdn.media.amplience.net/i/thegymgroup/The_Gym_Group-Exercises-How_To_Do_A_Russian_Twist-Step_By_Step_3?fmt=auto&h=545&w=1440&sm=c&qlt=default&$qlt$&$poi$', 'https://example.com/exercise10.mp4', 'Intermediate'),
+    ('Chest Press', 'Chest', 'A machine-based chest exercise to build chest muscles.', 'https://i0.wp.com/post.healthline.com/wp-content/uploads/2019/07/Male_Chest_Press_1296x728-header-1296x728.jpg?w=1155&h=1528', 'https://example.com/exercise11.mp4', 'Intermediate'),
+    ('Leg Press', 'Legs', 'A machine-based leg exercise to target quadriceps and glutes.', 'https://eurosportfitness.se/image/741/Animal-Leg-Press-2.jpg', 'https://example.com/exercise12.mp4', 'Intermediate'),
+    ('Lat Pulldown', 'Back', 'A machine exercise for building upper back and lats.', 'https://www.dmoose.com/cdn/shop/articles/feature-image_5640325e-b598-4308-a2e5-cd92450120c7.jpg?v=1676996563', 'https://example.com/exercise13.mp4', 'Intermediate'),
+    ('Tricep Extension', 'Arms', 'An isolation exercise for triceps development.', 'https://img-aws.ehowcdn.com/750x428p/contentlab-data.s3.amazonaws.com/getty/bb72f6a6cfb14a54915001ae5d8dcf83.jpg', 'https://example.com/exercise14.mp4', 'Intermediate'),
+    ('Mountain Climbers', 'Core', 'A dynamic core exercise that also provides cardio benefits.', 'https://cdn.mos.cms.futurecdn.net/JDdefEyTSFj9kt5QvjZqtB-1200-80.jpg', 'https://example.com/exercise15.mp4', 'Advanced'),
+    ('Calf Raises', 'Legs', 'An isolation exercise for strengthening calf muscles.', 'https://i.ytimg.com/vi/-M4-G8p8fmc/maxresdefault.jpg', 'https://example.com/exercise16.mp4', 'Intermediate'),
+    ('Seated Row', 'Back', 'A machine exercise for mid-back and rear deltoids.', 'https://weighttraining.guide/wp-content/uploads/2016/05/Seated-cable-row-new-resized.png', 'https://example.com/exercise17.mp4', 'Intermediate'),
+    ('Hammer Curls', 'Arms', 'A variation of bicep curls with a different grip.', 'https://cdn.shopify.com/s/files/1/1876/4703/files/shutterstock_419477203_480x480.jpg?v=1636560233', 'https://example.com/exercise18.mp4', 'Intermediate'),
+    ('Russian Kettlebell Swing', 'Core', 'A kettlebell exercise for core and hip power.', 'https://i.ytimg.com/vi/mKDIuUbH94Q/maxresdefault.jpg', 'https://example.com/exercise19.mp4', 'Intermediate'),
+    ('Leg Raises', 'Core', 'A bodyweight exercise to target lower abs.', 'https://www.mensjournal.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTk3MzIyNDgyODQyMDg0NjI5/legraises.jpg', 'https://example.com/exercise20.mp4', 'Beginner'),
+    ('Box Jumps', 'Legs', 'A plyometric exercise to improve leg explosiveness.', 'https://i.ytimg.com/vi/NBY9-kTuHEk/maxresdefault.jpg', 'https://example.com/exercise21.mp4', 'Advanced'),
+    ('Chin-up', 'Back', 'An underhand grip variation of the pull-up.', 'https://www.inspireusafoundation.org/wp-content/uploads/2023/04/chin-up-alternatives.jpg', 'https://example.com/exercise22.mp4', 'Intermediate'),
+    ('Skull Crushers', 'Arms', 'An isolation exercise to target the triceps.', 'https://cdn-0.weighttraining.guide/wp-content/uploads/2018/11/Barbell-skull-crusher-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4', 'https://example.com/exercise23.mp4', 'Advanced'),
+    ('Russian Twist with Medicine Ball', 'Core', 'A core exercise using a medicine ball for added resistance.', 'https://www.verywellfit.com/thmb/XMViMrZIc0mniQlbfrhe60XCGBM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/86-3120067-Seated-Oblique-twists-GIF-75816d0fb4c94494b254483a1fe1e365.gif', 'https://example.com/exercise24.mp4', 'Intermediate'),
+    ('Leg Curl', 'Legs', 'A machine-based exercise for hamstrings.', 'https://example.com/exercise25.jpg', 'https://example.com/exercise25.mp4', 'Beginner'),
+    ('Face Pull', 'Back', 'A rear deltoid and upper back exercise.', 'https://example.com/exercise26.jpg', 'https://example.com/exercise26.mp4', 'Intermediate'),
+    ('Diamond Push-up', 'Chest', 'A push-up variation that targets the triceps.', 'https://example.com/exercise27.jpg', 'https://example.com/exercise27.mp4', 'Intermediate'),
+    ('Bent-over Rows', 'Back', 'A compound exercise for the upper back and lats.', 'https://example.com/exercise28.jpg', 'https://example.com/exercise28.mp4', 'Advanced'),
+    ('Hamstring Curl', 'Legs', 'An isolation exercise for hamstring development.', 'https://example.com/exercise29.jpg', 'https://example.com/exercise29.mp4', 'Intermediate'),
+    ('Barbell Curl', 'Arms', 'A classic bicep exercise using a barbell.', 'https://example.com/exercise30.jpg', 'https://example.com/exercise30.mp4', 'Intermediate');
 
+--Workouts
 INSERT INTO tb_workout (workout_desc, workout_name)
 VALUES 
-    ('Test workout 1', 'Sweatifier'), ('Test workout 2', 'Pumppumppump'), ('Test workout 3', 'Weightwatcher');
+    ('A high-intensity interval training (HIIT) session that will make you sweat and burn calories.', 'Sweatifier'), 
+    ('A strength training workout focused on building muscle and increasing your pump.', 'Pumppumppump'), 
+    ('A weight loss program that combines cardio and strength exercises for effective results.', 'Weightwatcher'),
+    ('Improve your endurance with this cardio-focused workout that keeps your heart rate up.', 'Endurance Boost'),
+    ('Enhance your flexibility and relieve muscle tension with a combination of stretching exercises.', 'Flex and Stretch'),
+    ('Strengthen your core muscles with a series of exercises designed to build stability and power.', 'Core Strength Builder'),
+    ('Engage your entire body and burn calories with a full-body workout that targets all major muscle groups.', 'Full-body Burn'),
+    ('Crush calories and improve cardiovascular fitness with this intense cardio workout.', 'Cardio Crusher'),
+    ('Build balance and stability with exercises that challenge your core and coordination.', 'Balance and Stability'),
+    ('Improve functional fitness and prepare your body for daily activities with this workout.', 'Functional Fitness'),
+    ('Enhance power and explosiveness with a plyometric workout that focuses on rapid muscle contractions.', 'Power Plyometrics'),
+    ('Relax your mind and body with yoga and relaxation exercises that promote inner peace.', 'Yoga and Relaxation'),
+    ('Embrace the great outdoors and experience adventure with a variety of outdoor workouts.', 'Outdoor Adventure'),
+    ('Enhance your boxing and kickboxing skills with this intense and dynamic workout.', 'Boxing and Kickboxing'),
+    ('Boost your rowing endurance and strength with this challenging indoor rowing workout.', 'Rowing Challenge');
 
 INSERT INTO tb_goal (goal_name, start_date, end_date, completed)
 VALUES 
-    ('Week 1', '2023-09-04', '2023-09-10', false), ('Week 2', '2023-09-04', '2023-09-10', false), ('Week 3', '2023-09-04', '2023-09-10', false), ('Week 4', '2023-09-11', '2023-07-18', false);
-
-UPDATE tb_user SET current_goal_goal_id = 1 WHERE user_id = '58efed46-d459-4a14-b6bb-dec9b4240f94';
-UPDATE tb_user SET current_goal_goal_id = 2 WHERE user_id = '3542EF3';
-UPDATE tb_user SET current_goal_goal_id = 3 WHERE user_id = '23#45gs';
-
-UPDATE tb_goal SET user_id = '58efed46-d459-4a14-b6bb-dec9b4240f94' WHERE goal_id = 1;
-UPDATE tb_goal SET user_id = '23#45gs' WHERE goal_id = 3;
-UPDATE tb_goal SET user_id = '3542EF3' WHERE goal_id = 2;
+    ('Get Fit in 1 Week', '2023-09-01', '2023-09-07', false),
+    ('Weight Loss Challenge', '2023-09-10', '2023-09-16', false),
+    ('Muscle Building Kickstart', '2023-09-15', '2023-09-21', false),
+    ('Core Strength Boost', '2023-09-05', '2023-09-11', false),
+    ('Cardio Conditioning', '2023-09-03', '2023-09-09', false),
+    ('Flexibility Improvement', '2023-09-07', '2023-09-13', false),
+    ('Mindfulness Meditation', '2023-09-08', '2023-09-14', false),
+    ('Nutrition Overhaul', '2023-09-11', '2023-09-17', false),
+    ('HIIT Challenge', '2023-09-14', '2023-09-20', false),
+    ('Lean and Toned', '2023-09-16', '2023-09-22', false),
+    ('Bodyweight Mastery', '2023-09-19', '2023-09-25', false),
+    ('Strength and Power', '2023-09-21', '2023-09-27', false),
+    ('Full-Body Transformation', '2023-09-24', '2023-09-30', false),
+    ('Relaxation Journey', '2023-09-26', '2023-10-02', false),
+    ('Active Lifestyle', '2023-09-29', '2023-10-05', false);
 
 INSERT INTO tb_program (program_category, program_desc, program_img_url, program_name)
 VALUES 
-    ('Weight loss', 'Designed to lose weight.', 'https://example.com/program1.jpg', 'Weight loss of D00M!'), 
-    ('Muscle increase', 'Buff up!', 'https://example.com/program2.jpg', 'Get big!'), 
-    ('TestCategory', 'TestDesc', 'testURL', 'testName');
+    ('Weight Loss', 'A comprehensive program tailored to help you achieve your weight loss goals.', 'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/woman-measuring-stomach-1296x728.jpg?w=1155&h=1528', 'Weight Loss Transformation'),
+    ('Muscle Building', 'Unlock your full muscle-building potential with our scientifically proven program.', 'https://img.mensxp.com/media/content/2018/Aug/how-steroids-completely-change-the-game-of-muscle-building-and-getting-bigger1400-1534401287.jpg', 'Muscle Mass Mastery'),
+    ('Yoga and Meditation', 'Experience inner peace and physical vitality through our holistic program.', 'https://cdn.mos.cms.futurecdn.net/dXPfnrn9VoCyDpMctefx3d.jpg', 'Mindful Wellness Journey'),
+    ('HIIT Workouts', 'Get your heart rate up and burn calories fast with our high-intensity interval training program.', 'https://i.ytimg.com/vi/tgm_QjUiwlk/maxresdefault.jpg', 'HIIT Power Sessions'),
+    ('Flexibility Training', 'Increase your flexibility and prevent injuries with our flexibility-focused program.', 'https://musclematters.ca/wp-content/uploads/2020/01/45-The-Importance-of-Flexibility-Training.fi_.jpg', 'Flexible Body Revolution'),
+    ('Strength and Conditioning', 'Build strength, endurance, and lean muscle with our dynamic training program.', 'https://www.stack.com/wp-content/uploads/2018/04/30140000/High-School-Strength-Coach-STACK.jpg', 'Strength & Fitness Fusion'),
+    ('Mental Resilience', 'Enhance your mental toughness and resilience with our psychological well-being program.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh1RBXLQsFxNAxQb_cDY3zW5RVOnnWO2qHPQ&usqp=CAU', 'Mindset Mastery'),
+    ('Pilates for Core', 'Strengthen your core muscles and improve posture with our Pilates program.', 'https://media.self.com/photos/5a79d77a9f7fc74032b0448c/4:3/w_5000,h_3750,c_limit/011618_SELF_2845.jpg', 'Core Stability Challenge'),
+    ('Running and Endurance', 'Boost your running performance and endurance with our specialized program.', 'https://wpassets.trainingpeaks.com/wp-content/uploads/2018/08/07143848/18193-speed-vs-endurance-running-blog-700x394.jpg', 'Endurance Runner Pro'),
+    ('Mediterranean Diet', 'Adopt a healthy Mediterranean diet to improve your overall well-being.', 'https://images.everydayhealth.com/images/is-the-mediterranean-diet-best-for-diabetes-1440x810.jpg', 'Mediterranean Lifestyle'),
+    ('CrossFit Experience', 'Experience the intensity of CrossFit and achieve peak fitness levels.', 'https://uploads-ssl.webflow.com/61f6a1f670934ed39d548853/64b91cadcaedaf16a514a508_Crossfit%20Gym.webp', 'CrossFit Revolution'),
+    ('Mental Relaxation', 'Relax your mind, reduce stress, and improve focus with our mindfulness program.', 'https://www.verywellmind.com/thmb/6iakflImD6Pis-Pv3oDV-uwy4fw=/2122x0/filters:no_upscale():max_bytes(150000):strip_icc()/breathing-relax-ocean-standing-56b750a83df78c0b135f8ffa.jpg', 'Mindful Tranquility'),
+    ('Kickboxing Fitness', 'Get fit and unleash your inner fighter with our kickboxing fitness program.', 'https://dta0yqvfnusiq.cloudfront.net/burns43043213/2019/10/Kickboxing-is-the-Perfect-Full-Body-Workout-5db98bcbde358.jpg', 'Kickboxing Warrior'),
+    ('Outdoor Adventure', 'Combine fitness with adventure in our outdoor fitness program.', 'https://dcfwfuaf91uza.cloudfront.net/online/wp-content/uploads/Outdoor-Adventure-Courses-1.jpg', 'Adventure Fitness Expedition'),
+    ('Functional Fitness', 'Enhance your overall functional fitness and physical well-being.', 'https://www.trxtraining.com/cdn/shop/articles/Functional_Fitness.jpg?v=1645067939', 'Functional Fitness Challenge');
 
--- INSERT INTO tb_exercise_workouts (exercises_exercise_id, workouts_workout_id) 
--- VALUES
--- (1,2), (2,2), (3,2), (4,2), (5,2), (10,1), (9,1), (8,1), (7,1), (6,1);
-
+--Workout-Exercise Relationships
 INSERT INTO tb_workout_exercises (exercise_id, workout_id) 
 VALUES
-(1,2), (2,2), (3,2), (4,2), (5,2), (10,1), (9,1), (8,1), (7,1), (6,1);
+(1,1), (2,1), (3,1), (4,1), (5,1), (6,2), (7,2), (8,2), (9,2), (10,2), (11,3), (5,3), (13,3), (14,3), (15,3), (12,3), 
+(1,4), (14,4), (9,4), (6,4), (7,4), (10,5), (11,5), (4,5), (6,5), (12,6), (3,6), (8,6), (9,7), (3,7), (2,7), (1,7),
+(4,8), (3,8), (2,8), (5,5), (9,9), (10,9), (3,9), (5,10), (4,10), (2,10), (5,11),(8,11),(10,11),(2,12), (1,13), (8,13),
+(4,13), (4,14), (5,14), (5,4), (9,14), (10,14), (3,15), (5,15), (4,15), (2,6), (5,7), (8,8), (20,10), (20,2), (21,1), (27,8);
 
+--Goal-Program Relationships
 INSERT INTO tb_goal_programs (goal_id, program_id)
 VALUES 
-(2,1), (3,2);
-
+(1,1), (2,1), (3,1), (4,1), (5,1), (6,2), (7,2), (8,2), (9,2), (10,2), (11,3), (12,3), (13,3), (14,3), (15,3);
+--Goal-Workout Relationships
 INSERT INTO tb_goal_workouts (goal_id, workout_id)
 VALUES 
-(2,3), (3,2);
-
+(1,1), (2,1), (3,1), (4,1), (5,1), (6,2), (7,2), (8,2), (9,2), (10,2), (11,3), (12,3), (13,3), (14,3), (15,3);
+--Workout-Program Relationships
 INSERT INTO tb_workout_programs (workouts_workout_id, programs_program_id)
 VALUES
-(1,1), (2,1), (3,1), (1,2), (2,2), (3,2);
-
-INSERT INTO tb_goal_completed_workouts (completed_in_goal_goal_id, completed_workouts_workout_id)
-VALUES
-(1,2);
-
-INSERT INTO tb_goal_completed_programs (completed_in_goal_goal_id, completed_programs_program_id)
-VALUES
-(1,1), (1,2);
+(1,1), (2,1), (3,1), (4,1), (5,1), (6,2), (7,2), (8,2), (9,2), (10,2), (11,3), (12,3), (13,3), (14,3), (15,3);
